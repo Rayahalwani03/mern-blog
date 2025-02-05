857f5f506cb4a49c9d8ac048c117b45e667b0697
Rayahalwani03 <mira.res.mc@example.com>
2025-02-05 17:49:34 +0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
-import Express from 'express';
+import Express from 'express'; // the app framework
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; // for the env the env to not sow it in git
+import userRoute from'./route/user.route.js'; // js is most and the userRoute cosider it as var and the app will know 
 
-dotenv.config();
+dotenv.config(); // to use the .env file
 
 mongoose.connect(process.env.MONGO)
 .then(()=> {
@@ -16,5 +17,6 @@ const app = Express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-})
+}); // عم يشغل السيرفر 
     
+app.use('/api/user', (userRoute)); // we are going to use the get request
