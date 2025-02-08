8303df446c5c9cff340f8c9f3eceb59fb9b27a3f
Rayahalwani03 <mira.res.mc@example.com>
2025-02-08 21:18:28 +0300

    create signin api route

--- api/route/auth.route.js
+++ api/route/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { signup } from "../controllers/auth.crontroller.js";
+import { signin } from "../controllers/auth.crontroller.js";
 
 const router = express.Router();
 
 router.post('/signup', signup); //create 
+router.post('/signin', signin); //create 
+
 
 export default router;
