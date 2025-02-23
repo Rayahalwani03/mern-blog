4bae5fb662dbe18cfe279984055b521cbbe06de3
Rayahalwani03 <mira.res.mc@example.com>
2025-02-23 23:43:02 +0300

    delete user account fun

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -40,7 +40,7 @@ export const updateUser = async (req, res, next) => {
     if (req.body.password.length < 6) {
       return next(errorHandler(400, "password must be at least 6 characters"));
     }
-
+    
     req.body.password = await bcryptjs.hash(req.body.password, 10);
   }
 
@@ -101,7 +101,7 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next)=>{
   if(req.user.id !== req.params.userId){
-    return next(errorHandler(403, ' You are not allowed to delete this user'));
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
 
   try{
--- client/src/redux/user/useSlice.js
+++ client/src/redux/user/useSlice.js
@@ -1,47 +1,69 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser: null,
-    error: null,
-    loading: false
+  currentUser: null,
+  error: null,
+  loading: false,
 };
 
 const userSlice = createSlice({
-    name:'user',
-    initialState,
-    reducers:{
-        signInStart: (state)=>{
-            state.loading = true;
-            state.error = null;
-        },
-        signInSuccess: (state, action)=>{
-            state.currentUser = action.payload; // user details 
-            state.loading = false;
-            state.error = null;
-        },
-        signInFailure: (state, action)=>{ // no valid user 
-            state.loading = false;
-            state.error = action.payload;
-        },
-        updateStart: (state)=>{
-            state.loading = true;
-            state.error = null;
-        },
-        updateSuccess: (state, action)=>{
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        updateFailure: (state, action)=>{
-
-            state.loading = false;
-            state.error = action.payload;
-        },
-
-    }
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload; // user details
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      // no valid user
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null; /// we want to delete
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
-export const {signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure} = userSlice.actions; 
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
+} = userSlice.actions;
 
 //if we use export default, we could only export one thing.
 //we are exporting mutiple actions
@@ -49,6 +71,4 @@ export const {signInStart, signInSuccess, signInFailure, updateStart, updateSucc
 //With export default, you must import everything and rename manually
 //With named exports, you can only import what you need:
 
-
-
-export default userSlice.reducer; // we export as defult we can rename it later 
+export default userSlice.reducer; // we export as defult we can rename it later
