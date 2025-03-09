6b905bb43722a45f956e9d45a1eb86eb2dda45c2
Rayahalwani03 <mira.res.mc@example.com>
2025-03-09 23:35:47 +0300

    delete post func

--- api/route/post.route.js
+++ api/route/post.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { verifyToken } from "../../utils/verifyUser.js";
-import { create, getposts } from "../controllers/post.controller.js";
+import { create, deletepost, getposts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 
 router.post('/create',verifyToken, create); //post req in insomnia 
-router.get('/getposts', getposts)
-
+router.get('/getposts', getposts);
+router.delete('/deletepost/:postId/:userId',verifyToken, deletepost);
 //api/post/create
 
 export default router; // export it to index.js
