3f53b0ed4e185b317205478337e84ab37b7be5a0
Rayahalwani03 <mira.res.mc@example.com>
2025-03-12 00:24:21 +0300

    add delete user funcationality

--- api/route/user.route.js
+++ api/route/user.route.js
@@ -9,6 +9,5 @@ router.get("/test", test);
 router.put('/update/:userId', verifyToken, updateUser);    //put and post to update but put is better practice
 router.delete('/delete/:userId',verifyToken, deleteUser);
 router.post('/signout', signout);
-router.get('/getusers', verifyToken,getUsers) //prevent a random person from getting the information
-
+router.get('/getusers', verifyToken,getUsers); //prevent a random person from getting the information
 export default router;
