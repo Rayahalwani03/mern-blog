c9016091bb5f698c38b85dbbb77c4672d88a5370
Rayahalwani03 <mira.res.mc@example.com>
2025-03-13 00:24:36 +0300

    complete posts page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import SignUp from "./pages/SignUp";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/post/:postSlug" element={<PostPage />} />
         </Routes>
         <FooterCom />
       </BrowserRouter>
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,7 +2,7 @@
 const Projects = () => {
   return (
     <div>
-      p
+      
     </div>
   )
 }
