95b92b93874e5254c48f405f7b379a8a83523298
Rayahalwani03 <mira.res.mc@example.com>
2025-05-14 15:41:13 +0300

    about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,33 @@
-
 const About = () => {
   return (
-    <div>
-      about
-    </div>
-  )
-}
+    <div className=" min-h-screen flex items-start justify-center">
+      <div className="max-w-2xl max-auto p-3 text-center">
+        <div>
+          <h1 className=" text-3xl font-semibold text-center my-7">
+            About Ray's Blog
+          </h1>
 
-export default About
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Ray's Blog is a blog about Ray's life. Ray is a software engineer
+              and a startup founder.
+            </p>
+
+            <p className="">
+              Ray's Blog is a blog about Ray's life. Ray is a software engineer
+              and a startup founder.Ray's Blog is a blog about Ray's life. Ray
+              is a software engineer and a startup founder.
+            </p>
+
+            <p className="">
+              Ray's Blog is a blog about Ray's life. Ray is a software engineer
+              and a startup founder.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
